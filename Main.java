d20053a6c2fac713787d92d8e743a40aa88790bf
Dilnoza Akhrarova <121368213+dilnoza1984@users.noreply.github.com>
2022-12-30 03:33:44 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Main {
+  public static void main(String[] args) {
+   
+   
+System.out.println(trueNfalse(6));
+  
+
+  }
+  //create your method here
+
+public static boolean trueNfalse(int number){
+ 
+if (number % 2 == 0) {
+return true;
+}
+else {
+return false;
+}
+  
+}
+
+
+
+
+  
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Write a method that takes an integer and returns true if the num is even otherwise returns false.
+   Use  % operator to check for evenness or oddness
+
+   isEven(5); ->  //  false
+   isEven(10); -> //  true
